e1ddb12535ff01dfc69d4247087d887af598d2fb
Aryan Behzadi <aryan@grutto.com>
2025-12-21 00:55:50 +0000

    Remove conflicting pillow package before installing pillow-simd in setup script

--- setup.sh
+++ setup.sh
@@ -80,6 +80,8 @@ if [ "$BASIC" = true ] ; then
     pip install imageio imageio-ffmpeg tqdm easydict opencv-python-headless ninja trimesh transformers gradio==6.0.1 tensorboard pandas lpips zstandard
     pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8
     sudo apt install -y libjpeg-dev
+    # Uninstall regular pillow before installing pillow-simd to avoid conflicts
+    pip uninstall -y pillow 2>/dev/null || true
     pip install pillow-simd
     pip install kornia timm
 fi
